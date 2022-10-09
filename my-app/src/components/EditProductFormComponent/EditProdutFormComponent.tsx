76683fe758a886e5c74df7b11e3b021e8702fedb
Edvinas <edvinasalimas98@gmail.com>
2022-10-09 14:06:07 +0300

    SHOP-110 Implement state to be able to handle fields as form

--- my-app/src/components/EditProductFormComponent/EditProdutFormComponent.tsx
+++ my-app/src/components/EditProductFormComponent/EditProdutFormComponent.tsx
@@ -2,42 +2,64 @@ import { TextField } from "@mui/material";
 import React from "react";
 
 
-class EditProductFormComponent extends React.Component {
+class EditProductFormComponent extends React.Component<{}, {name: string}> {
+    constructor(props: {}) {
+        super(props)
+        this.state = {
+            name: ""
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event: { target: { value: string; }; }) {
+        this.setState({ name: event.target.value }); 
+       }
+
+   handleSubmit(event: any) {
+       alert('A name was submitted: ' + this.state.name);
+       event.preventDefault();
+   }
+
     render() {
         return (
             <div className="edit-product-form">
-                <div>
+                <form onSubmit={this.handleSubmit}>
                     <div>
-                        <TextField label="Name" variant="outlined" />
+                        <div>
+                            <TextField label="Name" variant="outlined" value={this.state.name} onChange={this.handleChange}/>
+                        </div>
+                        <div>
+                            <TextField label="Picture" variant="outlined" />
+                        </div>
+                        <div>
+                            <TextField label="Code" variant="outlined" />
+                        </div>
+                        <div>
+                            <TextField label="Price" variant="outlined" />
+                        </div>
+                        <div>
+                            <TextField label="Quantity" variant="outlined" />
+                        </div>
+                        <div>
+                            <TextField label="Type" variant="outlined" />
+                        </div>
+                        <div>
+                            <TextField label="Category" variant="outlined" />
+                        </div>
+                        <div>
+                            <TextField label="Sub Category" variant="outlined" />
+                        </div>
+                        <div>
+                            <TextField label="Stripe Product Id" variant="outlined" />
+                        </div>
+                        <div>
+                            <TextField label="Stripe Price Id" variant="outlined" />
+                        </div>
+                        <button type="submit">Submit</button>
                     </div>
-                    <div>
-                        <TextField label="Picture" variant="outlined" />
-                    </div>
-                    <div>
-                        <TextField label="Code" variant="outlined" />
-                    </div>
-                    <div>
-                        <TextField label="Price" variant="outlined" />
-                    </div>
-                    <div>
-                        <TextField label="Quantity" variant="outlined" />
-                    </div>
-                    <div>
-                        <TextField label="Type" variant="outlined" />
-                    </div>
-                    <div>
-                        <TextField label="Category" variant="outlined" />
-                    </div>
-                    <div>
-                        <TextField label="Sub Category" variant="outlined" />
-                    </div>
-                    <div>
-                        <TextField label="Stripe Product Id" variant="outlined" />
-                    </div>
-                    <div>
-                        <TextField label="Stripe Price Id" variant="outlined" />
-                    </div>
-                </div>
+                </form>
             </div>
         );
     }
