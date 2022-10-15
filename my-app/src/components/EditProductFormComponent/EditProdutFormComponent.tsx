12ec0d829969cd201eeaf3da5eebb3abe2ce6bee
Edvinas <edvinasalimas98@gmail.com>
2022-10-15 20:36:38 +0300

    SHOP-111 Add ability to pass props to child components

--- my-app/src/components/EditProductFormComponent/EditProdutFormComponent.tsx
+++ my-app/src/components/EditProductFormComponent/EditProdutFormComponent.tsx
@@ -1,5 +1,6 @@
 import { TextField } from "@mui/material";
 import React from "react";
+import { Form } from "react-router-dom";
 import { Product } from "../../models/product";
 import "../EditProductFormComponent/EditProductFormComponent.scss";
 
@@ -10,32 +11,7 @@ export interface ProductProps {
 class EditProductFormComponent extends React.Component<ProductProps, Product> {
     constructor(props: ProductProps) {
         super(props)
-        const { product } = this.props;
-        this.state = {
-            id: product.id,
-            name: product.name,
-            code: product.code,
-            picture: product.picture,
-            price: product.price,
-            quantity: product.quantity,
-            type: product.type,
-            category: product.category,
-            subCategory: product.subCategory,
-            stripeProductId: product.stripeProductId,
-            stripePriceId: product.stripePriceId,
-            errors: {
-                name: "",
-                code: "",
-                picture: "",
-                price: "",
-                quantity: "",
-                type: "",
-                category: "",
-                subCategory: "",
-                stripeProductId: "",
-                stripePriceId: ""
-            }
-        };
+        this.state = this.props.product;
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -51,7 +27,7 @@ class EditProductFormComponent extends React.Component<ProductProps, Product> {
         const requestOptions = {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(this.state)
+            body: JSON.stringify(this.props.product)
         };
         fetch("http://localhost:8081/api/shop/product", requestOptions);
     }
@@ -62,34 +38,34 @@ class EditProductFormComponent extends React.Component<ProductProps, Product> {
                 <form onSubmit={this.handleSubmit}>
                     <div className="edit-product-form-wrapper">
                         <div className="edit-product-form-input">
-                            <TextField name="name" label="Name" variant="outlined" value={this.state.name} onChange={this.handleChange} />
+                            <TextField name="name" label="Name" variant="outlined" value={this.props.product.name} onChange={this.handleChange} />
                         </div>
                         <div className="edit-product-form-input">
-                            <TextField name="picture" label="Picture" variant="outlined" value={this.state.picture} onChange={this.handleChange} />
+                            <TextField name="picture" label="Picture" variant="outlined" value={this.props.product.picture} onChange={this.handleChange} />
                         </div>
                         <div className="edit-product-form-input">
-                            <TextField name="code" label="Code" variant="outlined" value={this.state.code} onChange={this.handleChange} />
+                            <TextField name="code" label="Code" variant="outlined" value={this.props.product.code} onChange={this.handleChange} />
                         </div>
                         <div className="edit-product-form-input">
-                            <TextField name="price" type="number" label="Price" variant="outlined" value={this.state.price} onChange={this.handleChange} />
+                            <TextField name="price" type="number" label="Price" variant="outlined" value={this.props.product.price} onChange={this.handleChange} />
                         </div>
                         <div className="edit-product-form-input">
-                            <TextField name="quantity" type="number" label="Quantity" variant="outlined" value={this.state.quantity} onChange={this.handleChange} />
+                            <TextField name="quantity" type="number" label="Quantity" variant="outlined" value={this.props.product.quantity} onChange={this.handleChange} />
                         </div>
                         <div className="edit-product-form-input">
-                            <TextField name="type" label="Type" variant="outlined" value={this.state.type} onChange={this.handleChange} />
+                            <TextField name="type" label="Type" variant="outlined" value={this.props.product.type} onChange={this.handleChange} />
                         </div>
                         <div className="edit-product-form-input">
-                            <TextField name="category" label="Category" variant="outlined" value={this.state.category} onChange={this.handleChange} />
+                            <TextField name="category" label="Category" variant="outlined" value={this.props.product.category} onChange={this.handleChange} />
                         </div>
                         <div className="edit-product-form-input">
-                            <TextField name="subCategory" label="Sub Category" variant="outlined" value={this.state.subCategory} onChange={this.handleChange} />
+                            <TextField name="subCategory" label="Sub Category" variant="outlined" value={this.props.product.subCategory} onChange={this.handleChange} />
                         </div>
                         <div className="edit-product-form-input">
                             <TextField name="stripeProductId" label="Stripe Product Id" variant="outlined" value={this.state.stripeProductId} onChange={this.handleChange} />
                         </div>
                         <div className="edit-product-form-input">
-                            <TextField name="stripePriceId" label="Stripe Price Id" variant="outlined" value={this.state.stripePriceId} onChange={this.handleChange} />
+                            <TextField name="stripePriceId" label="Stripe Price Id" variant="outlined" value={this.props.product.stripePriceId} onChange={this.handleChange} />
                         </div>
                     </div>
                     <button type="submit">Submit</button>
--- my-app/src/components/ProductsComponent/ProductsComponent.tsx
+++ my-app/src/components/ProductsComponent/ProductsComponent.tsx
@@ -1,19 +1,29 @@
-import React from 'react';
-import './ProductsComponent.scss';
-import { DataGrid, GridColDef } from '@mui/x-data-grid';
-import { Box } from "@mui/material";
-import EditProductFormComponent from '../EditProductFormComponent/EditProdutFormComponent';
+import React from "react";
+import "./ProductsComponent.scss";
+import { Product } from "../../models/product";
+import EditProductFormComponent from "../EditProductFormComponent/EditProdutFormComponent";
 
 class ProductsComponent extends React.Component {
-
   state = {
     products: {
-      content: [], empty: false, first: false, last: false, number: 0, numberOfElements: 0, pageable:
-      {
-        offset: 0, pageNumber: 0, pageSize: 0, paged: false, sort:
-          { empty: false, sorted: false, unsorted: false }, unpaged: false
-      }, size: 0, sort:
-        { empty: false, sorted: false, unsorted: false }, totalElements: 0, totalPages: 0
+      content: [],
+      empty: false,
+      first: false,
+      last: false,
+      number: 0,
+      numberOfElements: 0,
+      pageable: {
+        offset: 0,
+        pageNumber: 0,
+        pageSize: 0,
+        paged: false,
+        sort: { empty: false, sorted: false, unsorted: false },
+        unpaged: false,
+      },
+      size: 0,
+      sort: { empty: false, sorted: false, unsorted: false },
+      totalElements: 0,
+      totalPages: 0,
     },
     loading: true,
     error: false,
@@ -39,78 +49,53 @@ class ProductsComponent extends React.Component {
         category: "",
         subCategory: "",
         stripeProductId: "",
-        stripePriceId: ""
-      }
-    }
-  }
-
-  private columns: GridColDef[] = [
-    {
-      field: 'id',
-      headerName: 'ID',
-      width: 90
+        stripePriceId: "",
+      },
     },
-    {
-      field: 'name',
-      headerName: 'Name',
-      width: 150,
-    },
-    {
-      field: 'picture',
-      headerName: 'Picture',
-      width: 150,
-    },
-    {
-      field: 'code',
-      headerName: 'Code',
-      width: 150,
-    },
-    {
-      field: 'price',
-      headerName: 'Price',
-      type: 'number',
-      width: 110,
-    },
-    {
-      field: 'quantity',
-      headerName: 'Quantity',
-      width: 150,
-    },
-    {
-      field: 'type',
-      headerName: 'Type',
-      width: 150,
-    },
-  ];
+  };
 
   componentDidMount() {
-    fetch('http://localhost:8081/api/shop/product')
-      .then(response => response.json())
-      .then(response => this.setState({
-        products: response,
-        loading: false
-      }))
-      .catch(error => this.setState({
-        loading: false,
-        error: true
-      }));
+    fetch("http://localhost:8081/api/shop/product")
+      .then((response) => response.json())
+      .then((response) =>
+        this.setState({
+          products: response,
+          loading: false,
+        })
+      )
+      .catch((error) =>
+        this.setState({
+          loading: false,
+          error: true,
+        })
+      );
+  }
+
+  editProduct(editableProduct: Product) {
+    this.setState({
+      product: editableProduct
+    });
+    console.log(this.state.product);
   }
 
   render() {
     return (
-      <Box sx={{ height: 400, width: '100%' }}>
-        {this.state.products !== undefined &&
-          <DataGrid
-            rows={this.state.products.content}
-            columns={this.columns}
-            pageSize={5}
-            rowsPerPageOptions={[5, 10, 15, 20]}
-            checkboxSelection
-            disableSelectionOnClick
-          />
-        }
-        <EditProductFormComponent product={this.state.product}></EditProductFormComponent>
-      </Box>
+      <div className="container">
+        <div className="list-of-products">
+          {this.state.products &&
+            this.state.products.content.map((product: Product) => (
+              <div key={product.id}>
+                <div>{product.name}</div>
+                <div>
+                  <button onClick={() => this.editProduct(product)}>Edit</button>
+                </div>
+              </div>
+            ))}
+        </div>
+        <EditProductFormComponent
+          product={this.state.product}
+        ></EditProductFormComponent>
+      </div>
     );
   }
 }
