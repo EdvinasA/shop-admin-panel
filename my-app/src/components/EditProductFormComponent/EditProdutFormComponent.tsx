ea7d5be357c093359af39e7ea09b3f74de4dc742
Edvinas <edvinasalimas98@gmail.com>
2022-10-16 16:33:50 +0300

    SHOP-111 Remove commented code

--- my-app/src/components/EditProductFormComponent/EditProdutFormComponent.tsx
+++ my-app/src/components/EditProductFormComponent/EditProdutFormComponent.tsx
@@ -7,65 +7,6 @@ export interface ProductProps {
     product: Product;
 }
 
-// const EditProductFormComponent = ({ product }: ProductProps) => {
-
-//     const [productState, setProductState] = useState(product);
-
-//     function handleChange(event: { target: { name?: any, value?: any }; }) {
-//         const { name, value } = event.target;
-//         setProductState({ [name]: value } as unknown as Product);
-//     }
-
-//     function handleSubmit(event: any) {
-//     event.preventDefault();
-//     const requestOptions = {
-//         method: 'PUT',
-//         headers: { 'Content-Type': 'application/json' },
-//         body: JSON.stringify(this.props.product)
-//     };
-//     fetch("http://localhost:8081/api/shop/product", requestOptions);
-// }
-
-//     return (
-//         <div className="edit-product-form">
-//             <form onSubmit={handleSubmit}>
-//                 <div className="edit-product-form-wrapper">
-//                     <div className="edit-product-form-input">
-//                         <TextField name="name" label="Name" variant="outlined" value={productState.name || ''} onChange={handleChange} />
-//                     </div>
-//                     <div className="edit-product-form-input">
-//                         <TextField name="picture" label="Picture" variant="outlined" value={productState.picture || ''} onChange={handleChange} />
-//                     </div>
-//                     <div className="edit-product-form-input">
-//                         <TextField name="code" label="Code" variant="outlined" value={productState.code || ''} onChange={handleChange} />
-//                     </div>
-//                     <div className="edit-product-form-input">
-//                         <TextField name="price" type="number" label="Price" variant="outlined" value={productState.price || ''} onChange={handleChange} />
-//                     </div>
-//                     <div className="edit-product-form-input">
-//                         <TextField name="quantity" type="number" label="Quantity" variant="outlined" value={productState.quantity || ''} onChange={handleChange} />
-//                     </div>
-//                     <div className="edit-product-form-input">
-//                         <TextField name="type" label="Type" variant="outlined" value={productState.type || ''} onChange={handleChange} />
-//                     </div>
-//                     <div className="edit-product-form-input">
-//                         <TextField name="category" label="Category" variant="outlined" value={productState.category || ''} onChange={handleChange} />
-//                     </div>
-//                     <div className="edit-product-form-input">
-//                         <TextField name="subCategory" label="Sub Category" variant="outlined" value={productState.subCategory || ''} onChange={handleChange} />
-//                     </div>
-//                     <div className="edit-product-form-input">
-//                         <TextField name="stripeProductId" label="Stripe Product Id" variant="outlined" value={productState.stripeProductId || ''} onChange={handleChange} />
-//                     </div>
-//                     <div className="edit-product-form-input">
-//                         <TextField name="stripePriceId" label="Stripe Price Id" variant="outlined" value={productState.stripePriceId || ''} onChange={handleChange} />
-//                     </div>
-//                 </div>
-//                 <button type="submit">Submit</button>
-//             </form>
-//         </div>
-//     );
-// }
 class EditProductFormComponent extends React.Component<ProductProps, Product> {
     constructor(props: ProductProps) {
         super(props)
